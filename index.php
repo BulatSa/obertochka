9dcfe67018e923cdb457e22146803c58e9d1e392
Bulat <sadikovba@gmail.com>
2018-10-26 13:48:40 +0300

    Initial

---
+++ index.php
@@ -0,0 +1,370 @@
+﻿<?php include('header.php'); ?>
+
+<section class="header-sec">
+	<div class="container">
+		<div class="header">
+			<div class="burger">
+				<a href="#"><span></span></a>
+			</div>
+			<div class="header__logo">
+				<img src="img/logo.svg" alt="Pen&Brain">
+			</div>
+			<div class="header__contact">
+				<div class="header__contact-phone">
+					<span>Казанский офис</span>
+					<a href="tel:+7 (843) 227-42-27">+7 (843) 227-42-27</a>
+				</div>
+				<div class="header__contact-phone">
+					<span>Московский офис</span>
+					<a href="tel:+7 (495) 000-00-00">+7 (495) 000-00-00</a>
+				</div>
+				<div class="header__contact-btn">
+					<a href="#" class="btn">Обсудить проект</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="main-info-sec">
+	<div class="main-info">
+		<div class="container">
+			<div class="main-info__head">
+				<div class="main-info__head-title">
+					<h1>
+						<span>Упаковано</span>
+						С 2013 года мы реализовали 251 проекта в области корпоративных сайтов, лендингов и интернет-магазинов
+					</h1>
+				</div>
+				<div class="main-info__head-tags">
+					<a href="#" class="active" data-filter="all">
+						<img src="img/icon-all.svg" alt="">
+						Все
+						<span>250</span>
+					</a>
+					<a href="#" data-filter=".category-a">Сайты<span>25</span></a>
+					<a href="#" data-filter=".category-b">Лендинги<span>120</span></a>
+					<a href="#">Магазины<span>12</span></a>
+					<a href="#">Строительство<span>12</span></a>
+					<a href="#">Красота и здоровье<span>4</span></a>
+					<a href="#">Мода<span>4</span></a>
+					<a href="#">Инфобизнес<span>3</span></a>
+					<a href="#">Haier</a>
+					<a href="#">Microsoft</a>
+					<a href="#">Фабрика Предпринимательства<span>2</span></a>
+					<a href="#">Рыбаков Фонд<span>1</span></a>
+					<a href="#">B2b<span>50</span></a>
+					<a href="#">B2c<span>10</span></a>
+					<a href="#">
+						<img src="img/icon-archive.svg" alt="">
+						Архив
+						<span>20</span>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="main-info__body">
+			<a href="#" class="main-info__link mix category-a" data-order="1">
+				<img src="img/items/item-1.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link main-info__link--white mix category-b" data-order="1">
+				<img src="img/items/item-2.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link mix category-a" data-order="2">
+				<img src="img/items/item-3.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link main-info__link--white mix category-b" data-order="2">
+				<img src="img/items/item-4.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link mix category-b" data-order="2">
+				<img src="img/items/item-5.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link main-info__link--white mix category-a" data-order="3">
+				<img src="img/items/item-6.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+
+			<a href="#" class="main-info__link main-info__link--white mix category-b" data-order="1">
+				<img src="img/items/item-2.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link mix category-a" data-order="2">
+				<img src="img/items/item-3.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link main-info__link--white mix category-b" data-order="2">
+				<img src="img/items/item-4.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link mix category-a" data-order="1">
+				<img src="img/items/item-1.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link main-info__link--white mix category-a" data-order="3">
+				<img src="img/items/item-6.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+
+			<a href="#" class="main-info__link mix category-b" data-order="2">
+				<img src="img/items/item-5.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link mix category-a" data-order="1">
+				<img src="img/items/item-1.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link main-info__link--white mix category-b" data-order="1">
+				<img src="img/items/item-2.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link mix category-a" data-order="2">
+				<img src="img/items/item-3.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link main-info__link--white mix category-b" data-order="2">
+				<img src="img/items/item-4.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link mix category-b" data-order="2">
+				<img src="img/items/item-5.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link main-info__link--white mix category-a" data-order="3">
+				<img src="img/items/item-6.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+
+
+			<a href="#" class="main-info__link main-info__link--white mix category-b" data-order="1">
+				<img src="img/items/item-2.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link mix category-a" data-order="2">
+				<img src="img/items/item-3.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link main-info__link--white mix category-b" data-order="2">
+				<img src="img/items/item-4.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link mix category-b" data-order="2">
+				<img src="img/items/item-5.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link mix category-a" data-order="1">
+				<img src="img/items/item-1.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link main-info__link--white mix category-a" data-order="3">
+				<img src="img/items/item-6.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+
+			<a href="#" class="main-info__link mix category-a" data-order="1">
+				<img src="img/items/item-1.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link main-info__link--white mix category-b" data-order="1">
+				<img src="img/items/item-2.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link mix category-a" data-order="2">
+				<img src="img/items/item-3.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link main-info__link--white mix category-a" data-order="3">
+				<img src="img/items/item-6.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link main-info__link--white mix category-b" data-order="2">
+				<img src="img/items/item-4.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+			<a href="#" class="main-info__link mix category-b" data-order="2">
+				<img src="img/items/item-5.jpg" alt="">
+				<p>
+					лендинг стоматологической клиники
+					<span>Top Smile</span>
+					<i class="i-icon-archive"></i>
+				</p>
+			</a>
+
+		</div>
+	</div>
+</section>
+
+
+<section class="footer-sec">
+	<div class="container">
+		<div class="footer">
+			<div class="footer__top">
+				<div class="footer__logo">
+					<img src="img/logo-footer.svg" alt="">
+				</div>
+				<div class="footer__contact-list">
+					<div class="footer__contact">
+						<span>Telegram I WhatsApp</span>
+						<a href="tel:+7 (903) 341-15-47">+7 (903) 341-15-47</a>
+					</div>
+					<div class="footer__contact">
+						<span>Оставить заявку</span>
+						<a href="mailto:mail@penbrain.ru">mail@penbrain.ru</a>
+					</div>
+					<div class="footer__contact">
+						<span>Казанский офис</span>
+						<a href="tel:+7 (843) 227-42-27">+7 (843) 227-42-27</a>
+						<span>Петербургская 50, корпус 23 офис 516/2</span>
+					</div>
+					<div class="footer__contact">
+						<span>Московский офис</span>
+						<a href="tel:+7 (495) 000-00-00">+7 (495) 000-00-00</a>
+						<span>Открытие в 2019 году</span>
+					</div>
+				</div>
+				<div class="footer__btn">
+					<a href="#" class="btn">Обсудить проект</a>
+				</div>
+			</div>
+			<div class="footer__bottom">
+				<div class="footer__copy">
+					<span>© 2013 — 2015 ООО «Пэн энд Брейн»</span>
+					<a href="#" target="_blank">Политика конфиденциальности</a>
+				</div>
+				<div class="footer__descr">
+					<p>Проекты, которые находятся на ссылках demo.penbrain.ru/NAME или NAME.penbrain.ru являются пробными версиями и не несут коммерческого, информационного смысла, а отражают лишь визуальную составляющую проектов клиентов агенствта.
+						Копирование проектов запрещено. </p>
+				</div>
+				<div class="footer__soc">
+					<a href="#" target="_blank"><i class="i-soc-vk"></i></a>
+					<a href="#" target="_blank"><i class="i-soc-fb"></i></a>
+					<a href="#" target="_blank"><i class="i-soc-you"></i></a>
+					<a href="#" target="_blank"><i class="i-soc-insta-2"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include('footer.php'); ?>
